56f29bf36f1e2365b7886afaa9988a3c23316a1c
fouadfouadbou99-boop <fouadfouadbou99@gmail.com>
2026-09-16 11:27:12 +0100

    Refactor chart plotting functions for clarity

--- utils/charts.py
+++ utils/charts.py
@@ -1,6 +1,11 @@
 import plotly.graph_objects as go
 
-def plot_chart(df, supports, resistances):
+
+def plot_main_chart(
+    df,
+    supports,
+    resistances
+):
 
     fig = go.Figure()
 
@@ -8,7 +13,8 @@ def plot_chart(df, supports, resistances):
         go.Scatter(
             x=df["Date"],
             y=df["Close"],
-            name="MASI"
+            name="MASI",
+            line=dict(width=3)
         )
     )
 
@@ -28,10 +34,104 @@ def plot_chart(df, supports, resistances):
         )
     )
 
+    fig.add_trace(
+        go.Scatter(
+            x=df["Date"],
+            y=df["SMA200"],
+            name="SMA200"
+        )
+    )
+
+    fig.add_trace(
+        go.Scatter(
+            x=df["Date"],
+            y=df["BB_UPPER"],
+            name="BB Upper",
+            line=dict(dash="dot")
+        )
+    )
+
+    fig.add_trace(
+        go.Scatter(
+            x=df["Date"],
+            y=df["BB_LOWER"],
+            name="BB Lower",
+            line=dict(dash="dot")
+        )
+    )
+
     for s in supports:
-        fig.add_hline(y=s, line_dash="dot")
+        fig.add_hline(
+            y=float(s),
+            line_color="green"
+        )
 
     for r in resistances:
-        fig.add_hline(y=r, line_dash="dash")
+        fig.add_hline(
+            y=float(r),
+            line_color="red"
+        )
+
+    return fig
+
+
+def plot_rsi_chart(df):
+
+    fig = go.Figure()
+
+    fig.add_trace(
+        go.Scatter(
+            x=df["Date"],
+            y=df["RSI"],
+            name="RSI"
+        )
+    )
+
+    fig.add_hline(y=70)
+    fig.add_hline(y=30)
+
+    return fig
+
+
+def plot_macd_chart(df):
+
+    fig = go.Figure()
+
+    fig.add_trace(
+        go.Scatter(
+            x=df["Date"],
+            y=df["MACD"],
+            name="MACD"
+        )
+    )
+
+    fig.add_trace(
+        go.Scatter(
+            x=df["Date"],
+            y=df["MACD_SIGNAL"],
+            name="Signal"
+        )
+    )
+
+    fig.add_bar(
+        x=df["Date"],
+        y=df["MACD_HIST"],
+        name="Histogramme"
+    )
+
+    return fig
+
+
+def plot_equity_curve(bt):
+
+    fig = go.Figure()
+
+    fig.add_trace(
+        go.Scatter(
+            y=bt["equity"],
+            mode="lines",
+            name="Equity Curve"
+        )
+    )
 
     return fig
